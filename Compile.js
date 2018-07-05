d44cec23f20affbd686e37561804673e19c6d693
David Kob <Davidkob@MacBook-Pro-59.local>
2018-07-05 18:41:52 -0400

    lottery upload

--- Compile.js
+++ Compile.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const solc = require('solc');
 
 //dirname always set to current working directory
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 
 // contract exporting is interface and the other is bytecode
-module.exports = solc.compile(source,1).contracts[':Inbox'];
+module.exports = solc.compile(source,1).contracts[':Lottery'];
